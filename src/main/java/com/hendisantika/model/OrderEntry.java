b84c882c51d4f2f423f955a32c8be68e6f7330d4
Hendi Santika <hendisantika@gmail.com>
2021-09-14 05:49:28 +0700

    #10 - Add OrderEntry Model

---
+++ src/main/java/com/hendisantika/model/OrderEntry.java
@@ -0,0 +1,19 @@
+package com.hendisantika.model;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jasper-report-pdf-generation-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/09/21
+ * Time: 05.49
+ */
+@Data
+public class OrderEntry {
+    private String productName;
+    private Integer quantity;
+    private Double price;
+}
